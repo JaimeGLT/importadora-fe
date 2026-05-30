5ced6ffcd93de4266fdb253e8a7614aef1af35cc
Jaime Guzman <jaimeguzmanlt@gmail.com>
2026-05-30 09:06:30 -0400

    proxy viejo

--- api/proxy.ts
+++ api/proxy.ts
@@ -1,6 +1,6 @@
 export const runtime = 'edge'
 
-const BACKEND = 'https://importadora-usa-grbkc0bah9adbher.chilecentral-01.azurewebsites.net'
+const BACKEND = 'https://usaautopartesapi20260406085513-amh4fwdnanbpa9gs.centralus-01.azurewebsites.net'
 // const BACKEND = "http://localhost:5120"
 
 function rewriteSetCookie(setCookieValue: string | null, targetDomain: string): string {
